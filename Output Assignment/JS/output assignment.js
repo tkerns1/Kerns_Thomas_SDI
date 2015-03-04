7ff330ffb719bb33abc2e80f783b318315acb28f
tkerns1 <takerns@fullsail.edu>
2015-03-04 12:58:06 -0500

    statement
    added another statement

--- Output Assignment/JS/output assignment.js
+++ Output Assignment/JS/output assignment.js
@@ -8,6 +8,7 @@ var shoeSize = 10.5; // number variable
 var myClass = "SDI"; //string variable
 var status = true;   // Boolean variable
 
+console.log("Hello Class");
 console.log("My shoe size is " + shoeSize + ".");
 console.log("My current class is " + myClass + ".");
 console.log(" It's " + status + " I had a problem with Github.");
