61e7d41f793721ab1bc0798a6d4d7e23dd22d32f
tkerns1 <takerns@fullsail.edu>
2015-03-05 11:24:02 -0500

    bill
    completed code for grocery bill program

---
+++ Expressions_worksheet/JS/bill.js
@@ -0,0 +1,12 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * External Worksheets
+ */
+
+var weeklyBill = [120, 130, 140, 100, 85 ]
+var totalBills = weeklyBill[0] + weeklyBill[1] + weeklyBill[2] + weeklyBill[3] + weeklyBill[4];
+var averageBill = totalBills/5
+
+console.log("Your average bill is " + averageBill + " but " + totalBills + " is your total for 5 weeks");
