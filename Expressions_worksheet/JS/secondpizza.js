91462b7c5f8be12987d364926b5244e9214c3333
tkerns1 <takerns@fullsail.edu>
2015-03-05 10:57:24 -0500

    second pizza
    completed and tested

---
+++ Expressions_worksheet/JS/secondpizza.js
@@ -0,0 +1,13 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * External Worksheets
+ */
+
+var partyPeople = 10;
+var orderedPizzas = 4;
+var slicesPer = 8;
+var sparksSlices = (slicesPer * orderedPizzas)/10;
+
+console.log("Sparks share if the pizza slices was " + sparksSlices + " slices");
