2778217b0f04d5ef96c96122b59ca6a3f0893565
tkerns1 <takerns@fullsail.edu>
2015-03-05 10:16:43 -0500

    add variables

---
+++ Expressions_worksheet/JS/dogyears.js
@@ -0,0 +1,12 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * External Worksheet
+ *
+ */
+var realYears = 3;
+var dogYears = 7;
+var sparksAge = realYears * dogYears;
+
+console.log("Sparky is " + realYears + " in human years. But he is " + sparksAge + " in dog years");
