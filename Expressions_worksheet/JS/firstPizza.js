04dc943fb4702b5a8055f52d16cd02be24e9e903
tkerns1 <takerns@fullsail.edu>
2015-03-05 10:41:04 -0500

    first pizza
    completed code

--- Expressions_worksheet/JS/firstPizza.js
+++ Expressions_worksheet/JS/firstPizza.js
@@ -8,4 +8,7 @@
 var partyPeople = 10;
 var orderedPizzas = 3;
 var slicesPizza = orderedPizzas * 8;
+var slicesPer = slicesPizza/10;
 
+console.log("Each person ate " + slicesPer + " slices of pizza at the party")
+console.log("there were " + partyPeople + " at the party" )
