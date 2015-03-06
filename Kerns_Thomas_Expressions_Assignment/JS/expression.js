ddb1fac80242cd835407ff3bd165f2949b08437d
tkerns1 <takerns@fullsail.edu>
2015-03-06 17:50:26 -0500

    changed code
    added console command

--- Kerns_Thomas_Expressions_Assignment/JS/expression.js
+++ Kerns_Thomas_Expressions_Assignment/JS/expression.js
@@ -5,3 +5,11 @@
 var woodPile = New Array()
 
 woodPile[0] = prompt("please enter the amount of wood left");
+woodPile[1] = prompt("please enter the amount of wood left in pile two")
+woodPile[2] = prompt("please enter the amount of wood left in pile three")
+
+var total = number(woodPile[0]) + number(woodPile[1]) + number(woodPile[2]);
+
+var avarage = total/3;
+
+console.log("")
