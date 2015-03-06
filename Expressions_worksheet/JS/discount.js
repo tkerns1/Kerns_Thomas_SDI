3638c8f5fadbf99a9158cbe1d2ab91f2cf8f24c7
tkerns1 <takerns@fullsail.edu>
2015-03-06 17:41:49 -0500

    expression
    started code

---
+++ Expressions_worksheet/JS/discount.js
@@ -0,0 +1,16 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * External Worksheets
+ */
+
+var item = "shirt";
+var originalPrice = 10;
+var discount    =   20;
+var salesTax = 8;
+var discountedPrice = originalPrice * discount%;
+var withTax = (originalPrice * discount%) * salesTax%;
+
+console.log("Your " + item + " was originally $" + originalPrice + "after the discount $" + discountedPrice + " and ") ;
+//" before taxes, after taxes" + withTax + "is your total");
---
+++ Kerns_Thomas_Expressions_Assignment/JS/expression.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ThomasKerns on 3/6/15.
+ */
+
+var woodPile = New Array()
+
+woodPile[0] = prompt("please enter the amount of wood left");
