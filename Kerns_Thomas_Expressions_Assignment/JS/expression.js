5e6d967c3a1afc16940f4123cd0b2ef267043988
tkerns1 <takerns@fullsail.edu>
2015-03-06 18:05:06 -0500

    code complete
    code worked just needs comments

--- Kerns_Thomas_Expressions_Assignment/JS/expression.js
+++ Kerns_Thomas_Expressions_Assignment/JS/expression.js
@@ -2,13 +2,13 @@
  * Created by ThomasKerns on 3/6/15.
  */
 
-var woodPile = New Array();
+var woodPile = new Array();
 
 woodPile[0] = prompt("please enter the amount of wood left");
 woodPile[1] = prompt("please enter the amount of wood left in pile two");
 woodPile[2] = prompt("please enter the amount of wood left in pile three");
 
-var total = number(woodPile[0]) + number(woodPile[1]) + number(woodPile[2]);
+var total = Number(woodPile[0]) + Number(woodPile[1]) + Number(woodPile[2]);
 
 var average = total/3;
 
