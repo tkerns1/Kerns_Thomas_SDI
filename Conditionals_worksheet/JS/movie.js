ad14c097af5a9c57bec65977d4a4ddadc85b8a31
tkerns1 <takerns@fullsail.edu>
2015-03-12 09:03:15 -0400

    worksheet
    completed worksheets

---
+++ Conditionals_worksheet/JS/movie.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ThomasKerns on 3/12/15.
+ */
+
+    var age = 30;
+    var time = 7;
+        if (age >=55 && age <=10 || time >=3 && time <=5){
+            var ticketPrice = 7.00;
+            console.log ("your ticket price is " + ticketPrice + " dollars.")
+        }
+            else{
+            var ticketPrice = 12.00;
+            console.log ("Your ticket price is $" + ticketPrice )
+        }
