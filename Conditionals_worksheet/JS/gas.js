86e9cd0a4c66a55534fc8cfcca5639b588fdd650
tkerns1 <takerns@fullsail.edu>
2015-03-12 08:30:35 -0400

    worksheets

---
+++ Conditionals_worksheet/JS/gas.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ThomasKerns on 3/11/15.
+ */
+
+var gallons = 20;
+var mpg = 30;
+var total = (gallons * mpg );
+
+if(gallons >= 15){
+    console.log("keep driving, you have " + gallons + " gallons left and a total of " + total + "miles left till empty")
+}
---
+++ Conditionals_worksheet/JS/grade.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ThomasKerns on 3/12/15.
+ */
+
+
+var letterGrade = 45;
+
+if (letterGrade >=90){
+    console.log("You have a " + letterGrade + "% You got an A!");
+}
+    else if (letterGrade >=80){
+        console.log ("You have a " + letterGrade + "% You got a B!");
+}
+        else if (letterGrade >=70){
+            console.log("You have a " + letterGrade + "% you do a C!");
+}
+            else if (letterGrade >=60){
+                console.log("You have a " + letterGrade + "% You got a D!");
+}
+                else{
+                    console.log("You have a " + letterGrade + "% You got an F!")
+}
+
+  //  console.log("You have a" + letterGrade + "% ")
