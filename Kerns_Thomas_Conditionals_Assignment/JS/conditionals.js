53a22c8a0c284041f4ce3c45cbefe9eb66806c69
tkerns1 <takerns@fullsail.edu>
2015-03-12 10:54:05 -0400

    variables
    started variables

---
+++ Kerns_Thomas_Conditionals_Assignment/JS/conditionals.js
@@ -0,0 +1,10 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * Conditionals assignment
+ */
+
+var height = prompt("enter the height of the box: ");
+var length = prompt ("enter the length of the box:");
+var width  = prompt("enter the width of the box:");
