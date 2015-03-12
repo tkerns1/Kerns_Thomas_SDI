6549624f2c49032f3dc4261b2a6cdbff1692f925
tkerns1 <takerns@fullsail.edu>
2015-03-12 11:07:21 -0400

    added code

--- Kerns_Thomas_Conditionals_Assignment/JS/conditionals.js
+++ Kerns_Thomas_Conditionals_Assignment/JS/conditionals.js
@@ -5,6 +5,10 @@
  * Conditionals assignment
  */
 
-var height = prompt("enter the height of the box: ");
-var length = prompt ("enter the length of the box:");
-var width  = prompt("enter the width of the box:");
+var height = prompt("enter the height of the box in inches: ");
+var length = prompt ("enter the length of the box in inches:");
+var width  = prompt("enter the width of the box in inches:");
+var volume = Number(height) * Number(length) * Number(width);
+basePrice = 12.00
+
+(volume <1000) ? console.log("you can ship your box") : console.log("Your box is too big");
