16844837dadffc1a746a87c7cdf7afc2c7437964
tkerns1 <takerns@fullsail.edu>
2015-03-17 13:51:08 -0400

    problem 2

--- Functions_worksheet/JS/functions.js
+++ Functions_worksheet/JS/functions.js
@@ -8,9 +8,17 @@
 
 function circom(){
     var radius = 5;// radius of the circle
-    var circom = 2 * 3.14 * radius; the formula for circomference of a circle
-    console.log("The circumfrence of your circle is " + circom + " sq ft")
+    var circom = 2 * 3.14 * radius; //the formula for circomference of a circle
+    console.log("The circumfrence of your circle is " + circom + " sq ft") //print out of the circumference
 }
-circom();
+circom();// calling the function up
 
 console.log("------------------Bee Stings-----------------------");
+
+function beeStings(){
+    var stingers = 8.666666667;
+    var weight = 300;
+    var tillDeath = stingers * weight;
+    console.log("It takes " + tillDeath + " bee stings to kill an animal this size")
+}
+beeStings();
