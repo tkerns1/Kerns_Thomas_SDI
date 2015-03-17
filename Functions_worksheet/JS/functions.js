7af38d3f15ae3958815d045f93369a2833a3a969
tkerns1 <takerns@fullsail.edu>
2015-03-17 13:31:04 -0400

    worksheet

---
+++ Functions_worksheet/JS/functions.js
@@ -0,0 +1,15 @@
+/**
+ * Thomas Kerns
+ * SDI
+ * 201503-02
+ * Function Worksheet
+ */
+// example area
+
+function dogYears(){
+    var age = 4;
+    var dogYears = age * 7;
+    console.log ( "Sparky is " + dogYears + " in dog years")
+}
+
+dogYears();
