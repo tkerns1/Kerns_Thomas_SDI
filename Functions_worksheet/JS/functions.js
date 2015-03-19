71aecd4a5719263fe1af859edcf21d91489174d1
tkerns1 <takerns@fullsail.edu>
2015-03-19 14:48:22 -0400

    fixed

--- Functions_worksheet/JS/functions.js
+++ Functions_worksheet/JS/functions.js
@@ -6,19 +6,35 @@
  */
 // example area
 
-function circom(){
     var radius = 5;// radius of the circle
-    var circom = 2 * 3.14 * radius; //the formula for circomference of a circle
-    console.log("The circumfrence of your circle is " + circom + " sq ft") //print out of the circumference
+    //var circom = 2 * 3.14 * radius; //the formula for circomference of a circle
+    var circomference = (2 * 3 * radius);
+    console.log("The circumfrence of your circle is " + circomference + " sq ft"); //print out of the circumference
+
+function circom(){
+    return 2 * 3 * radius
 }
-circom();// calling the function up
+// calling the function up
 
 console.log("------------------Bee Stings-----------------------");
 
-function beeStings(){
-    var stingers = 8.666666667;
-    var weight = 300;
-    var tillDeath = stingers * weight;
-    console.log("It takes " + tillDeath + " bee stings to kill an animal this size")
+
+    var stingers = 8.666666667; // number of bee sting per pound needed to kill something
+    var weight = 300;// weight of animal
+    var tillDeath = (stingers * weight); // number of stingers times the weight of the animal
+    console.log("It takes " + tillDeath + " bee stings to kill an animal this size") // print of of number of stingers
+
+    function totalStingers(stingers, weight){ //declaring the function name
+        return stingers * weight;// returning the value of the function
+    }
+
+console.log("--------------------example--------------------------");
+
+var width = 5
+var height = 6
+var area = calculateArea(width, height);
+console.log("the area of the retangle is " + area + " sq ft.");
+
+function calculateArea(w, h){
+    return w * h;
 }
-beeStings();
