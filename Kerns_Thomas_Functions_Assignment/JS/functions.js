4e49f33dac652d7a0d0ca70679851c37c2824682
tkerns1 <takerns@fullsail.edu>
2015-03-20 17:28:41 -0400

    finished with code

--- Kerns_Thomas_Functions_Assignment/JS/functions.js
+++ Kerns_Thomas_Functions_Assignment/JS/functions.js
@@ -7,21 +7,29 @@
  */
 
 //var prompt
-var userPrompt = Number(prompt("Enter 1 for florida lottery or 2 for power ball"));
-while(userPrompt === ""){
-    userPrompt = prompt("Please enter a number");
-}
-//IF statement
-// florida lotto
-if (userPrompt === 1){
+var userPrompt =prompt("Enter 1 for florida lottery or 2 for power ball");
+    while(userPrompt === ""){
 
-    function lottery() {
-       for (var a = 0; a<=5; a++){
-           var lottery = Math.round(Math.random() * (53 - 1) + 1);
-           console.log (lottery);
-       }
+        userPrompt = prompt("Please enter a number");
     }
-    console.log ("your Florida Lottery numbers are: ")
+function pbNumbers() {
+    for (var b = 0; b <= 4; b++) {
+        var pbNumbers = Math.round(Math.random() * (59 - 1) + 1);
+        console.log(pbNumbers);
+    }
+}
+function lottery() {
+    for (var a = 0; a<=5; a++){
+        var lottery = Math.round(Math.random() * (53 - 1) + 1);
+        console.log (lottery);
+    }
+}
+
+//IF statement
+if (userPrompt == 1){
+
+
+    console.log ("your Florida Lottery numbers are: ");
     lottery();
 
 }
@@ -31,16 +39,11 @@ if (userPrompt === 1){
 
 //ELSE statement
  else {
-    function pbNumbers(){
-        for (var b = 0; b<=4; b++){
-            var pbNumbers = Math.round(Math.random() * (59 - 1) + 1);
-            console.log(pbNumbers);
-        }
-    }
-    console.log ("Your powerball numbers are: ");
+
+
+    console.log("Your powerball numbers are: ");
     pbNumbers();
     var powerBall = Math.round(Math.random() * (35 - 1) + 1);
     console.log("Your powerball number is: " + powerBall)
-}// powerball
-//while
-// function for powerball number
+
+}
