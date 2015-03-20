19e39fceb133510ed9cee09cca7f151aa0590e58
tkerns1 <takerns@fullsail.edu>
2015-03-20 18:01:42 -0400

    comments
    finished comments

--- Kerns_Thomas_Functions_Assignment/JS/functions.js
+++ Kerns_Thomas_Functions_Assignment/JS/functions.js
@@ -7,43 +7,40 @@
  */
 
 //var prompt
-var userPrompt =prompt("Enter 1 for florida lottery or 2 for power ball");
-    while(userPrompt === ""){
+var userPrompt =prompt("Enter 1 for florida lottery or 2 for power ball");// creates a user prompt
+    while(userPrompt === ""){//validates the user prompt for empty responses
 
-        userPrompt = prompt("Please enter a number");
+        userPrompt = prompt("Please enter a number");// validates
     }
-function pbNumbers() {
-    for (var b = 0; b <= 4; b++) {
-        var pbNumbers = Math.round(Math.random() * (59 - 1) + 1);
-        console.log(pbNumbers);
+function pbNumbers() { // creates a function for powerball lotto
+    for (var b = 0; b <= 4; b++) { // sets the amount of times it will run
+        var pbNumbers = Math.round(Math.random() * (59 - 1) + 1); //creates a rounded off random number
+        console.log(pbNumbers);// outputs a number per run
     }
 }
-function lottery() {
-    for (var a = 0; a<=5; a++){
-        var lottery = Math.round(Math.random() * (53 - 1) + 1);
-        console.log (lottery);
+function lottery() { //creates a function called lottery
+    for (var a = 0; a<=5; a++){// determines how many times it will run
+        var lottery = Math.round(Math.random() * (53 - 1) + 1); //creates a rounded off random number between 1 - 53
+        console.log (lottery);// outputs a number per run
     }
 }
 
 //IF statement
-if (userPrompt == 1){
+if (userPrompt == 1){// option one for users too choose
 
 
-    console.log ("your Florida Lottery numbers are: ");
-    lottery();
+    console.log ("Your Florida Lottery numbers are: ");// output to the user
+    lottery();// calls up the function lottery
 
 }
 
-
-
-
 //ELSE statement
- else {
+ else {// second choice for the user to pick
 
 
-    console.log("Your powerball numbers are: ");
-    pbNumbers();
-    var powerBall = Math.round(Math.random() * (35 - 1) + 1);
-    console.log("Your powerball number is: " + powerBall)
+    console.log("Your powerball numbers are: ");// output to the user
+    pbNumbers();// calls up the function pbNumbers
+    var powerBall = Math.round(Math.random() * (35 - 1) + 1);// generates a random number between 1-35
+    console.log("Your powerball number is: " + powerBall) //displays the variable powerBall to the user
 
 }
