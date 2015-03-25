fd1ae10ed990c20788a45aeef853dfba8666daf9
tkerns1 <takerns@fullsail.edu>
2015-03-25 15:40:27 -0400

    reworked code

--- Kerns_Thomas_Functions_Assignment/JS/functions.js
+++ Kerns_Thomas_Functions_Assignment/JS/functions.js
@@ -6,39 +6,41 @@
  *
  */
 
-//var prompt
 var userPrompt =prompt("Enter 1 for florida lottery or 2 for power ball");// creates a user prompt
-    while(userPrompt === ""){//validates the user prompt for empty responses
+while(userPrompt === ""){//validates the user prompt for empty responses
 
-        userPrompt = prompt("Please enter a number");// validates
-    }
-function pbNumbers() { // creates a function for powerball lotto
-    for (var b = 0; b <= 4; b++) { // sets the amount of times it will run
-        var pbNumbers = Math.round(Math.random() * (59 - 1) + 1); //creates a rounded off random number
-        console.log(pbNumbers);// outputs a number per run
-    }
+    userPrompt = prompt("Please enter a number");// validates
 }
-function lottery() { //creates a function called lottery
-    for (var a = 0; a<=5; a++){// determines how many times it will run
-        var lottery = Math.round(Math.random() * (53 - 1) + 1); //creates a rounded off random number between 1 - 53
-        console.log (lottery);// outputs a number per run
+function lottery(max, min, num){
+    var randomArray = [];// new array
+    for (var a = 0; a < num; a++) {// number of runs based on num
+        var myLotto = Math.round(Math.random() * (max - min) + min)// establishs the random number
+        randomArray[a] = myLotto; //places the random number in the array
+        console.log(myLotto); // prints out the random number
     }
 }
 
-//IF statement
-if (userPrompt == 1){// option one for users too choose
+function pbNumbers(max, min, num){
+    var randomArray = [];// new array
+    for (var a = 0; a < num; a++) {// number of runs
+        var myPball = Math.round(Math.random() * (max - min) + min)//makes the random number
+        randomArray[a] = myPball;// attaches the random number to the array
+        console.log(myPball);// prints out the random number
+    }
+}
+
+if (userPrompt == 1) {// option one for users too choose
 
 
-    console.log ("Your Florida Lottery numbers are: ");// output to the user
+    console.log("Your Florida Lottery numbers are: ");// output to the user
     lottery();// calls up the function lottery
-
+    lottery(53, 1, 6); // sets up the number of runs and max and min
 }
-
-//ELSE statement
- else {// second choice for the user to pick
+else {// second choice for the user to pick
 
 
     console.log("Your powerball numbers are: ");// output to the user
+    pbNumbers(59, 1, 5); // sets up the number of runs
     pbNumbers();// calls up the function pbNumbers
     var powerBall = Math.round(Math.random() * (35 - 1) + 1);// generates a random number between 1-35
     console.log("Your powerball number is: " + powerBall) //displays the variable powerBall to the user
